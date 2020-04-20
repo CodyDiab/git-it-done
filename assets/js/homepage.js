5e633fed2dd520ee5c766e0fdf833ab79066c55a
Diab <emailcodydiab@gmail.com>
2020-04-20 18:50:47 -0500

    created ground function for api retrieval

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+   
+/// declare var as place holder perameter for the fetch
+    var apiUrl = "https://api.github.com/users/" + user + "/repos"
+
+    fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    });
+  })
 };
 
 getUserRepos()
