40d6be3f106c3da833636d47d9bdf53b393cbebd
Diab <emailcodydiab@gmail.com>
2020-04-22 17:23:06 -0500

    created warning if repo contained more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning")
+
 var getRepoIssues = function(repo) {
    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc"
 
@@ -9,6 +11,10 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data){
                 // pass response to dom
                 displayIssues(data);
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         } else {
             alert("there was a problem with your request!")
@@ -20,7 +26,7 @@ var displayIssues = function(issues) {
     if (displayIssues.length === 0 ) {
         issueContainerEl.textContent = "This repo has no open issues!"
     }
-    for(var i = 0; i < displayIssues.length; i++) {
+    for(var i = 0; i < issues.length; i++) {
         // create a link for each object in array
         var issueEl = document.createElement("a");
         //apply style class
@@ -53,4 +59,16 @@ var displayIssues = function(issues) {
     }
 
 }
-getRepoIssues("codydiab/git-it-done")
+  // var to add text to warning container
+var displayWarning = function(repo) {
+     
+    limitWarningEl.textContent = "to see more than 30 issues, visit "
+    
+    var linkEl = document.createElement("a");
+    linkEl.textContent =(" See More Issues on GitHub.com")
+    linkEl.setAttribute("href","https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target","_blank");
+    limitWarningEl.appendChild(linkEl)
+
+}
+getRepoIssues("facebook/react")
